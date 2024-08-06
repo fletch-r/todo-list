e3c4b52e8eb83fe1170b6711bc52994cdedef26b
fletch-r <ath3ris@proton.me>
2024-08-06 14:46:57 +0100

    Initial Commit

---
+++ todo-list/todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_listApp.swift
+//  todo-list
+//
+//  Created by Andrew Fletcher on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
